4e12827a3490e5f261e0bb8ef7cb9ef3461e76c2
SbangariGIT <56751089+sbangariGIT@users.noreply.github.com>
2021-05-28 03:41:20 +0530

    responsive carousel

--- script.js
+++ script.js
@@ -29,5 +29,16 @@ $('.carousel').owlCarousel({
     autoplayHoverPause: true,
     dots: true,
     nav: true,
+    responsive:{
+        0:{
+            items: 1,
+        },
+        600:{
+            items: 2,
+        },
+        1000:{
+            items: 3,
+        }
+    }
     
 });
